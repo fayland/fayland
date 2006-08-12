ac960cf6d292e5e1a27c1a4b80f4253a7c91bf09
fayland <fayland@gmail.com>
2006-08-12 07:09:50 +0000

    set user_id in session table.

--- Foorum/lib/Foorum/Schema/Session.pm
+++ Foorum/lib/Foorum/Schema/Session.pm
@@ -5,7 +5,7 @@ use base qw/DBIx::Class/;
 __PACKAGE__->load_components(qw/PK::Auto Core/);
 __PACKAGE__->table('session');
 __PACKAGE__->add_columns(qw/
-    id session_data expires
+    id session_data expires user_id
 /);
 __PACKAGE__->set_primary_key('id');
 
