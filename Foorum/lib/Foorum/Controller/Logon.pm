3df1769f278a92d5b0a4d3d1fd28a79912b60569
fayland <fayland@gmail.com>
2006-09-29 02:46:26 +0000

    I don't have much time to code. damn it!

--- Foorum/lib/Foorum/Controller/Logon.pm
+++ Foorum/lib/Foorum/Controller/Logon.pm
@@ -12,59 +12,59 @@ sub begin : Private {
 }
 
 sub login : Global {
-	my ( $self, $c ) = @_;
-	
-	$c->stash->{template} = 'user/login.html';
-	return unless ($c->req->param('process'));
+    my ( $self, $c ) = @_;
+    
+    $c->stash->{template} = 'user/login.html';
+    return unless ($c->req->param('process'));
 
-	if ( my $username = $c->req->param("username") and my $password = $c->req->param("password") ) {
-	 	if ( $c->login( $username, $password ) ) {
-	 	    
-	 	    # check if he is actived
-	 	    if ($c->user->active_code) {
-	 	        my $username = $c->user->username;
-	 	        $c->logout;
-	 	        return $c->res->redirect("/register/activation/$username");
-	 	    }
-	 	
-		# remember me
-		if ($c->req->param('remember_me')) {
-			$c->session_time_to_live( 157680000 );
-		}
+    if ( my $username = $c->req->param("username") and my $password = $c->req->param("password") ) {
+        if ( $c->login( $username, $password ) ) {
+            
+            # check if he is actived
+            if ($c->user->active_code) {
+                my $username = $c->user->username;
+                $c->logout;
+                return $c->res->redirect("/register/activation/$username");
+            }
+        
+            # remember me
+            if ($c->req->param('remember_me')) {
+                $c->session_time_to_live( 31536000 ); # 1 Year = 24 * 60 * 60 * 365
+            }
 
-	 		# login_times++
-	 		$c->user->update( {
-	 		    login_times   => $c->user->login_times + 1,
-	 		    last_login_on => \"NOW()",
-	 		    last_login_ip => $c->req->address,
-	 		} );
-	 		
-	 		# redirect
-	 		my $referer = $c->session->{url_referer};
-	 		$c->log->debug("Refer to: " . $referer);
-     		if ($referer) {
-     		    $c->res->redirect($c->req->base . $referer);
-     		} else {
-     		    $c->res->redirect('/board');
-     		}
-	 	} else {
-	 	    $c->stash->{error} = 'ERROR_AUTH_FAILED';
-	 	}
-	 } else {
-	 	$c->stash->{error} = 'ERROR_ALL_REQUIRED';
-	 }
+            # login_times++
+            $c->user->update( {
+                login_times   => $c->user->login_times + 1,
+                last_login_on => \"NOW()",
+                last_login_ip => $c->req->address,
+            } );
+            
+            # redirect
+            my $referer = $c->session->{url_referer};
+            $c->log->debug("Refer to: " . $referer);
+            if ($referer) {
+                $c->res->redirect($c->req->base . $referer);
+            } else {
+                $c->res->redirect('/board');
+            }
+        } else {
+            $c->stash->{error} = 'ERROR_AUTH_FAILED';
+        }
+     } else {
+        $c->stash->{error} = 'ERROR_ALL_REQUIRED';
+     }
 }
 
 sub logout : Global {
-	my ( $self, $c ) = @_;
-	
-	# delete the user_id in session
-	$c->delete_session('log out');
-	
-	# log the user out
-	$c->logout;
+    my ( $self, $c ) = @_;
+    
+    # delete the user_id in session
+    $c->delete_session('log out');
+    
+    # log the user out
+    $c->logout;
 
-	$c->res->redirect('/');
+    $c->res->redirect('/');
 }
 
 =pod
