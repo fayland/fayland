170b77b0de36d1ecb2cf0b09e54b0606aeef0ec8
fayland <fayland@gmail.com>
2006-11-09 01:58:53 +0000

    404 error

--- Foorum/lib/Foorum/Controller/Root.pm
+++ Foorum/lib/Foorum/Controller/Root.pm
@@ -4,17 +4,15 @@ use strict;
 use warnings;
 use base 'Catalyst::Controller';
 use Time::HiRes qw( gettimeofday tv_interval );
+use URI::Escape;
 
 __PACKAGE__->config->{namespace} = '';
-
-# for elapsed_time
-use vars qw/$t0/;
 
 sub auto : Private {
 	my ( $self, $c ) = @_;
 	
 	# in case (begin : Private) is overrided
-	$t0 = [gettimeofday] unless ($t0);
+	$c->stash->{start_t0} = [gettimeofday] unless ($c->stash->{start_t0});
 	
 	# internationalization
 #    if ($c->req->headers->{'accept-language'} =~ /zh\-cn/) {
@@ -25,10 +23,9 @@ sub auto : Private {
 
 	my $path = $c->req->path;	
 	# for maintain, but admin can login and do something
-	if ($c->config->{maintain} and
-	    ($path ne 'login' and $path !~ /^admin\//)) {
-	        $c->stash->{template} = 'simple/maintain.html';
-	        return 0;
+	if ($c->config->{maintain} and $path !~ /^(admin|login)\//) {
+        $c->stash->{template} = 'simple/maintain.html';
+        return 0;
 	}
     
     return 1;
@@ -37,12 +34,20 @@ sub auto : Private {
 sub begin : Private {
     my ( $self, $c ) = @_;
     
-    $t0 = [gettimeofday];
+    $c->stash->{start_t0} = [gettimeofday];
 }
 
 sub default : Private {
     my ( $self, $c ) = @_;
 
+    # 404
+    $c->res->status(404);
+    $c->detach('/print_error', [ 'ERROR_404' ] );
+}
+
+sub index : Private {
+    my ( $self, $c ) = @_;
+    
     $c->forward('Foorum::Controller::Forum', 'default');
 }
 
@@ -51,7 +56,9 @@ sub end : Private {
     
     # for login using!
     if ($c->res->location and $c->res->location =~ /^\/login/) {
-        $c->res->location('/login?referer=/' . $c->req->path);
+        my $location = '/login?goto=/' . $c->req->path;
+        $location .= '?' . uri_escape($c->req->uri->query) if ($c->req->uri->query);
+        $c->res->location($location);
     }
 	return if ($c->res->body || $c->res->redirect);
 
@@ -62,14 +69,13 @@ sub end : Private {
     if ($c->stash->{template} =~ /^simple\//) {
         $c->stash->{simple_wrapper} = 1;
     } else {
-        
-        $c->stash->{elapsed_time} = tv_interval( $t0, [gettimeofday] );
-        
         # get whos view this page?
         unless ($c->stash->{no_online_view}) {
             my $results = $c->model('Online')->whos_view_this_page($c);
             $c->stash->{whos_view_this_page} = $results;
         }
+        
+        $c->stash->{elapsed_time} = tv_interval( $c->stash->{start_t0}, [gettimeofday] );
     }
     $c->forward( $c->view('TT') );
 }
