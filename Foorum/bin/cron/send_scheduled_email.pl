e0cc0d5b544b5223de224a3ff3b0eb0129ebd7aa
fayland <fayland@gmail.com>
2007-05-20 10:49:12 +0000

    send_scheduled_email.pl as daemon

--- Foorum/bin/cron/send_scheduled_email.pl
+++ Foorum/bin/cron/send_scheduled_email.pl
@@ -1,12 +1,13 @@
 #!/usr/bin/perl
 
-# perl send_scheduled_email.pl set as backend
+# daemon: perl send_scheduled_email.pl
 
 use strict;
 use warnings;
 use Data::Dumper;
 use FindBin qw/$Bin/;
 use lib "$Bin/../../lib";
+use Proc::Daemon;
 
 use YAML qw(LoadFile);
 use Foorum::Schema; # DB Schema
@@ -25,22 +26,32 @@ my $schema = Foorum::Schema->connect(
     { AutoCommit => 1, RaiseError => 1, PrintError => 1 },
 );
 
-# 2592000 = 30 * 24 * 60 * 60
-my $rs = $schema->resultset('ScheduledEmail')->search( { processed => 'N' } );
+# Daemonize
+Proc::Daemon::Init();
 
-my $handled = 0;
-while (my $rec = $rs->next) {
+while (1) {
+
+    my $start_time = time();
+
+    my $rs = $schema->resultset('ScheduledEmail')->search( { processed => 'N' } );
     
-    print 'To: ' . $rec->to_email . ' Subject: ' . $rec->subject . "\n";
-    send_email( $rec->from_email, $rec->to_email, $rec->subject, $rec->plain_body, $rec->html_body );
+    while (my $rec = $rs->next) {
+        
+        print 'To: ' . $rec->to_email . ' Subject: ' . $rec->subject . "\n";
+        send_email( $rec->from_email, $rec->to_email, $rec->subject, $rec->plain_body, $rec->html_body );
+        
+        # update processed
+        $rec->update( { processed => 'Y' } );
+    }
     
-    # update processed
-    $rec->update( { processed => 'Y' } );
-    
-    $handled++;
+    my $end_time = time();
+
+    if ($end_time - $start_time < 30) {
+        sleep 30;
+    } else {
+        sleep 5;
+    }
 }
-
-print "$0 - handled: $handled \@ " . localtime() . "\n";
 
 use MIME::Entity;
 use Mail::Mailer qw(sendmail);
