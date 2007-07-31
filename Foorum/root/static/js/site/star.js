80f77bc96567e4511111355a924b47ad75485864
fayland <fayland@gmail.com>
2007-07-31 15:24:28 +0000

    jquery part two

--- Foorum/root/static/js/site/star.js
+++ Foorum/root/static/js/site/star.js
@@ -1,22 +1,13 @@
-var star_obj_div;
 function star(obj_type, obj_id, obj_div) {
     var url = '/ajax/star';
     var pars = 'obj_type=' + obj_type + '&obj_id=' + obj_id;
     star_obj_div = obj_div;
 
-    var myAjax = new Ajax.Request( url, {
-	    method: 'get',
-	    parameters: pars,
-	    onSuccess: star_response,
-	    onFailure: function () { alert('ERROR') }
-	} );
-}
-
-function star_response(request) {
-    response  = request.responseText;
-    if (response == 1) {
-        Element.update(star_obj_div , '<img src="http://mail.google.com/mail/images/star_on_2.gif" />');
-    } else {
-        Element.update(star_obj_div , '<img src="http://mail.google.com/mail/images/star_off_2.gif" />');
-    }
+    $.get('/ajax/star', { 'obj_type': obj_type, 'obj_id': obj_id }, function(data) {
+        if (data == 1) {
+            $('#' + star_obj_div).html('<img src="http://mail.google.com/mail/images/star_on_2.gif" />');
+        } else {
+            $('#' + star_obj_div).html('<img src="http://mail.google.com/mail/images/star_off_2.gif" />');
+        }
+    } );
 }
