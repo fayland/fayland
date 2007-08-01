4cbdb7dea63cb94e431aa3d44caccd507c3121d8
fayland <fayland@gmail.com>
2007-08-01 14:34:06 +0000

    jquery

--- Foorum/root/static/js/utils.js
+++ Foorum/root/static/js/utils.js
@@ -1,14 +1,20 @@
-/*
-    Web Site:  http://dynamicdrive.com
-*/
-function disableForm(theform) {
-    if (document.all || document.getElementById) {
-        for (i = 0; i < theform.length; i++) {
-            var tempobj = theform.elements[i];
-            if (tempobj.type.toLowerCase() == "submit" || tempobj.type.toLowerCase() == "reset") {
-                tempobj.disabled = true;
-            }
+$(document.forms).each( function(theform) {
+    
+    // disabled the Submit and Reset when submit a form
+    // to avoid duplicate submit
+    $(theform).submit( function() {
+        $('input:submit').attr( { disabled : 'disabled' } );
+        $('input:reset').attr(  { disabled : 'disabled' } );
+    } );
+    
+    // Press Ctrl+Enter to submit the form. like QQ.
+    $(theform).keypress( function(evt) {
+        var x = evt.keyCode;
+        var q = evt.ctrlKey;
+        
+        if (q && ( x == 13 || x == 10)) {
+            theform.submit();
         }
-    }
-    return true;
-}
+    } );
+    
+} );
