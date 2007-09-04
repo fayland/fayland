4e290f3bc1bc03ced11a01160fe03a1d44553915
fayland <fayland@gmail.com>
2007-09-04 21:41:15 +0000

    change Catalyst-Model-DBIC-Schema-QueryLog

--- CPAN/Catalyst-Model-DBIC-Schema-QueryLog/lib/Catalyst/Model/DBIC/Schema/QueryLog.pm
+++ CPAN/Catalyst-Model-DBIC-Schema-QueryLog/lib/Catalyst/Model/DBIC/Schema/QueryLog.pm
@@ -7,17 +7,31 @@ $VERSION = '0.01';
 
 use base 'Catalyst::Model::DBIC::Schema';
 __PACKAGE__->mk_accessors('querylog');
+__PACKAGE__->mk_accessors('_querylog_analyzer');
 
 use DBIx::Class::QueryLog;
 use DBIx::Class::QueryLog::Analyzer;
+use Moose;
+with 'Catalyst::Component::InstancePerContext';
 
-sub new {
-    my $self = shift->NEXT::new(@_);
+sub querylog_analyzer {
+    my $self = shift;
+    
+    unless ($self->_querylog_analyzer) {
+        $self->_querylog_analyzer( new DBIx::Class::QueryLog::Analyzer({ querylog => $self->querylog }) );
+    }
+    
+    return $self->_querylog_analyzer;
+}
 
+sub build_per_context_instance {
+    my ($self, $c) = @_;
+    
     my $schema = $self->schema;
     
     my $querylog = new DBIx::Class::QueryLog();
     $self->querylog($querylog);
+    $self->_querylog_analyzer( undef );
     
     $schema->storage->debugobj( $querylog );
     $schema->storage->debug(1);
@@ -25,19 +39,12 @@ sub new {
     return $self;
 }
 
-sub querylog_analyzer {
-    my $self = shift;
-    
-    my $ann = new DBIx::Class::QueryLog::Analyzer({ querylog => $self->querylog });
-    return $ann;
-}
-
 1; # End of Catalyst::Model::DBIC::Schema::QueryLog
 __END__
 
 =head1 NAME
 
-Catalyst::Model::DBIC::Schema::QueryLog - The great new Catalyst::Model::DBIC::Schema::QueryLog!
+Catalyst::Model::DBIC::Schema::QueryLog - DBIx::Class::QueryLog Model Class
 
 =head1 SYNOPSIS
 
@@ -53,6 +60,10 @@ Catalyst::Model::DBIC::Schema::QueryLog - The great new Catalyst::Model::DBIC::S
                         {AutoCommit => 1}
                       ]
   );
+
+=head1 DESCRIPTION
+
+Generally, you should check the document of L<Catalyst::Model::DBIC::Schema>. this module use base of it, and only provide extra two methods below.
 
 =head1 METHODS
 
@@ -72,12 +83,12 @@ an instance of DBIx::Class::QueryLog::Analyzer.
 
   <div class="featurebox">
     <h3>Query Log Report</h3>
-    [% SET total = c.model('DBIC').querylog.time_elapsed | format('%0.6f') %]
+    [% SET total = c.model('FilmDB').querylog.time_elapsed | format('%0.6f') %]
     <div>Total SQL Time: [% total | format('%0.6f') %] seconds</div>
-    [% SET qcount = c.model('DBIC').querylog.count %]
+    [% SET qcount = c.model('FilmDB').querylog.count %]
     <div>Total Queries: [% qcount %]</div>
     [% IF qcount %]
-    <div>Avg Statement Time: [% (c.model('DBIC').querylog.time_elapsed / qcount) | format('%0.6f') %] seconds.</div>
+    <div>Avg Statement Time: [% (c.model('FilmDB').querylog.time_elapsed / qcount) | format('%0.6f') %] seconds.</div>
     <div>
      <table class="table1">
       <thead>
@@ -92,7 +103,7 @@ an instance of DBIx::Class::QueryLog::Analyzer.
         <th>SQL</th>
        </tr>
        [% SET i = 0 %]
-       [% FOREACH q = c.model('DBIC').querylog_analyzer.get_sorted_queries %]
+       [% FOREACH q = c.model('FilmDB').querylog_analyzer.get_sorted_queries %]
        <tr class="[% IF loop.count % 2 %]odd[% END %]">
         <th class="sub">[% q.time_elapsed | format('%0.6f') %]
         <td>[% ((q.time_elapsed / total ) * 100 ) | format('%i') %]%</td>
@@ -114,6 +125,8 @@ an instance of DBIx::Class::QueryLog::Analyzer.
 L<Catalyst::Model::DBIC::Schema>
 
 L<DBIx::Class::QueryLog>
+
+L<Catalyst::Component::InstancePerContext>
 
 =head1 AUTHOR
 
--- CPAN/Catalyst-Plugin-DBIC-QueryLog/t/00-load.t
+++
@@ -1,9 +0,0 @@
-#!perl -T
-
-use Test::More tests => 1;
-
-BEGIN {
-	use_ok( 'Catalyst::Plugin::DBIC::QueryLog' );
-}
-
-diag( "Testing Catalyst::Plugin::DBIC::QueryLog $Catalyst::Plugin::DBIC::QueryLog::VERSION, Perl $], $^X" );
