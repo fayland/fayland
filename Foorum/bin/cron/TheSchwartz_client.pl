ce2cdd0e518a638a28cc3de1fd75c39a73f44869
fayland <fayland@gmail.com>
2007-10-04 11:34:59 +0000

    Schedule::Cron

--- Foorum/bin/cron/TheSchwartz_client.pl
+++ Foorum/bin/cron/TheSchwartz_client.pl
@@ -4,9 +4,7 @@
 
 use strict;
 use warnings;
-use Data::Dump qw/dump/;
-use FindBin qw/$Bin/;
-use lib "$Bin/../../lib";
+use Schedule::Cron;
 
 # for both Linux/Win32
 my $has_proc_pid_file = eval "use Proc::PID::File; 1;";
@@ -16,11 +14,24 @@ if ($has_proc_pid_file) {
         exit(0);
     }
 }
+
+use FindBin qw/$Bin/;
+use lib "$Bin/../../lib";
 use Foorum::ExternalUtils qw/theschwartz/;
 
 my $client = theschwartz();
 
-$client->insert('Foorum::TheSchwartz::Worker::Hit'); # run every 5 minutes
-$client->insert('Foorum::TheSchwartz::Worker::RemoveOldDataFromDB'); # run everyday
+sub update_hit { # run every 5 minutes
+    $client->insert('Foorum::TheSchwartz::Worker::Hit');
+}
+
+sub remove_old_data_from_db { # run everyday
+    $client->insert('Foorum::TheSchwartz::Worker::RemoveOldDataFromDB');
+}
+
+my $cron =  new Schedule::Cron(sub { return 1;});
+$cron->add_entry("*/5 * * * *", \&update_hit);
+$cron->add_entry("1 0 * * *", \&remove_old_data_from_db);
+$cron->run();
 
 1;
