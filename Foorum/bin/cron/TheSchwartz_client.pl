4741e3922f739a89b7521c6a778b3cf9b28125b0
fayland <fayland@gmail.com>
2007-10-04 11:51:38 +0000

    avoid duplication.

--- Foorum/bin/cron/TheSchwartz_client.pl
+++ Foorum/bin/cron/TheSchwartz_client.pl
@@ -20,10 +20,16 @@ use Foorum::ExternalUtils qw/theschwartz/;
 my $client = theschwartz();
 
 sub update_hit { # run every 5 minutes
-    $client->insert('Foorum::TheSchwartz::Worker::Hit');
+    my @already_has = $client->list_jobs( { funcname => 'Foorum::TheSchwartz::Worker::Hit' } );
+    unless (scalar @already_has) {
+        $client->insert('Foorum::TheSchwartz::Worker::Hit');
+    }
 }
 sub remove_old_data_from_db { # run everyday
-    $client->insert('Foorum::TheSchwartz::Worker::RemoveOldDataFromDB');
+    my @already_has = $client->list_jobs( { funcname => 'Foorum::TheSchwartz::Worker::RemoveOldDataFromDB' } );
+    unless (scalar @already_has) {
+        $client->insert('Foorum::TheSchwartz::Worker::RemoveOldDataFromDB');
+    }
 }
 
 my $cron =  new Schedule::Cron(sub { return 1;});
