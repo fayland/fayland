9151a38653f784767cf526afc9ca80defb1ff1cc
fayland <fayland@gmail.com>
2007-12-06 22:26:53 +0000

    out 0.1.0 later

--- Foorum/lib/Foorum.pm
+++ Foorum/lib/Foorum.pm
@@ -24,7 +24,7 @@ use Catalyst qw/
 #
 
 use vars qw/$VERSION/;
-$VERSION = '0.08';
+$VERSION = '0.1.0';
 
 __PACKAGE__->config( { VERSION => $VERSION } );
 
@@ -32,18 +32,16 @@ __PACKAGE__->setup();
 
 __PACKAGE__->log->levels( 'error', 'fatal' );    # for real server
 if ( __PACKAGE__->config->{debug_mode} ) {
-
     __PACKAGE__->log->enable( 'debug', 'info', 'warn' )
         ;                                        # for developer server
     {
-
         # these code are copied from Catalyst.pm setup_log
         no strict 'refs';
         my $class = __PACKAGE__;
         *{"$class\::debug"} = sub {1};
     }
 
-    #my @extra_plugins = qw/ StackTrace /;        # DBIC::Schema::Profiler
+    #my @extra_plugins = qw/ StackTrace /;
     #__PACKAGE__->setup_plugins( [@extra_plugins] );
 }
 
