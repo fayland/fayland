f4fee32b30fd23f4c12bf873b7dc41bd130674e0
fayland <fayland@gmail.com>
2008-09-04 10:54:43 +0000

    add todo for tomorrow.

--- CPAN/MooseX-TheSchwartz/lib/MooseX/TheSchwartz/Job.pm
+++ CPAN/MooseX-TheSchwartz/lib/MooseX/TheSchwartz/Job.pm
@@ -1,10 +1,21 @@
 package MooseX::TheSchwartz::Job;
 
 use Moose;
+use Moose::Util::TypeConstraints;
+use Storable ();
+
+subtype 'Args'
+    => as 'Any'
+    => where { 1; };
+coerce 'Args'
+    => from 'Str'
+    => via { _cond_thaw($_) }
+    => from 'ScalarRef'
+    => via { Storable::thaw($$_) };
 
 has 'jobid'         => ( is => 'rw', isa => 'Int' );
 has 'funcid'        => ( is => 'rw', isa => 'Int' );
-has 'arg'           => ( is => 'rw', isa => 'Any' );
+has 'arg'           => ( is => 'rw', isa => 'Args' );
 has 'uniqkey'       => ( is => 'rw', isa => 'Maybe[Str]' );
 has 'insert_time'   => ( is => 'rw', isa => 'Maybe[Int]' );
 has 'run_after'     => ( is => 'rw', isa => 'Int', default => time() );
@@ -23,6 +34,28 @@ sub as_hashref {
     }
 
     \%data;
+}
+
+sub completed {
+    my $self = shift;
+    
+    
+}
+
+sub _cond_thaw {
+    my $data = shift;
+
+    my $magic = eval { Storable::read_magic($data); };
+    if ($magic && $magic->{major} && $magic->{major} >= 2 && $magic->{major} <= 5) {
+        my $thawed = eval { Storable::thaw($data) };
+        if ($@) {
+            # false alarm... looked like a Storable, but wasn't.
+            return $data;
+        }
+        return $thawed;
+    } else {
+        return $data;
+    }
 }
 
 1;
