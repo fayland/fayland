bc302d6e0ad0c1461de6960316b0361ea940ec92
fayland <fayland@gmail.com>
2008-09-11 11:19:10 +0000

    for an ship Acme::CPANAuthors::Chinese 0.01

--- CPAN/Acme-CPANAuthors-Chinese/lib/Acme/CPANAuthors/Chinese.pm
+++ CPAN/Acme-CPANAuthors-Chinese/lib/Acme/CPANAuthors/Chinese.pm
@@ -7,12 +7,31 @@ our $VERSION = '0.01';
 
 use Acme::CPANAuthors::Register (
     AGENT       => 'Agent Zhang (章亦春)',
+    CHAOSLAW    => '王晓哲',
+    CHENYR      => 'Chen Yirong (春江)',
     CHUNZI      => 'Chunzi',
     CHYLLI      => 'chylli',
+    CNHACKTNT   => '王晖',
+    DONGXU      => 'Dongxu Ma <马东旭>',
     FAYLAND     => 'Fayland 林',
     FKIORI      => '陈正伟',
     FLW         => '王兴华',
+    FOOLFISH    => '錢宇/Qian Yu',
+    HGNENG      => '黄冠能',
+    HOOWA       => '孙冰',
+    ISLUE       => '胡海麟',
+    JOEJIANG    => '蒋永清',
+    JWU         => '吴健源',
+    JZHANG      => '张军',
+    KAILI       => '李凯',
+    MAIN        => '吴健源',
+    PANGJ       => 'Jeff Pang',
+    QJZHOU      => 'Qing-Jie Zhou',
+    RANN        => '灿烂微笑 / Ran Ningyu',
+    SAL         => 'Sal Zhong (仲伟祥)',
+    WEIQK       => '万朝伟',
     YEWENBIN    => '叶文彬',
+    ZHUZHU      => 'Zhu Zhu',
 );
 
 1;
@@ -34,28 +53,26 @@ Acme::CPANAuthors::Chinese - CPAN 中国作者
 
    my $number   = $authors->count;
    my @ids      = $authors->id;
-   my @distros  = $authors->distributions('FGLOCK');
-   my $url      = $authors->avatar_url('GARU');
-   my $kwalitee = $authors->kwalitee('FCO');
+   my @distros  = $authors->distributions('AGENT');
+   my $url      = $authors->avatar_url('FAYLAND');
+   my $kwalitee = $authors->kwalitee('YEWENBIN');
 
 
 =head1 DESCRIPTION
 
 This class is used to provide a hash of chinese CPAN author's PAUSE id/name to Acme::CPANAuthors.
 
-Essa classe é usada para fornecer um hash de id/nome PAUSE de autores brasileiros no CPAN para o Acme::CPANAuthors.
-
 =head1 MAINTENANCE
 
-If you are a chinese CPAN author not listed here, please send me your id/name via email, IRC, or RT so we can always keep this module up to date. If there's amistake and you're listed here but are not chinese (or just don't want to be listed), sorry for the inconvenience: please contact me and I'll remove the entry right away.
-
-Se você é um autor brasileiro no CPAN e n?o está listado aqui, por favor me envie seu id/nome via email, IRC, ou RT para que possamos manter esse módulo sempre atualizado. Se houve um erro e você está listado aqui mas n?o é brasileiro (ou simplesmente n?o quer ser listado), desculpe a inconveniencia: por favor entre em contato que removerei a entrada imediatamente.
+If you are a chinese CPAN author not listed here, please send me your id/name via email or RT so we can always keep this module up to date. If there's a mistake and you're listed here but are not chinese (or just don't want to be listed), sorry for the inconvenience: please contact me and I'll remove the entry right away.
 
 =head1 SEE ALSO
 
-L<Acme::CPANAuthors> - Main class to manipulate this one (Classe principal para manipular esta)
+L<Acme::CPANAuthors> - Main class to manipulate this one
 
-L<Acme::CPANAuthors::Japanese> - Code and documentation nearly taken verbatim from it (Código e documenta??o copiadas daqui quase verbatim)
+L<Acme::CPANAuthors::Japanese> - Code and documentation nearly taken verbatim from it
+
+L<Acme::CPANAuthors::Brazilian> - inspired me directly
 
 =head1 AUTHOR
 
