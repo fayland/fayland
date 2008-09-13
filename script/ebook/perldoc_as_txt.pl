893fec0dbba0265644e68065d3341d8e73f6395d
fayland <fayland@gmail.com>
2008-09-13 07:08:01 +0000

    ebook/perldoc_as_txt.pl

---
+++ script/ebook/perldoc_as_txt.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Next;
+
+my @modules = @ARGV;
+
+my $save_text;
+foreach my $module (@modules) {
+    my $file = find_module_place($module);
+    unless ($file) {
+        warn "$module is missing, please retry\n";
+        next;
+    }
+    # get all the sub-modules
+    my @sub_modules_files = get_sub_modules($file);
+    unshift @sub_modules_files, $file;
+    
+    # merge all files' source into a txt file
+    foreach my $pm (@sub_modules_files) {
+        open(my $fh, '<', $pm);
+        local $/ = undef;
+        my $text = <$fh>;
+        close($fh);
+        $save_text .= "=" x 50 . "\n$pm\n$text\n";
+    }
+}
+
+if ($save_text) {
+    open(my $fh, '>', $$ . ".txt");
+    print $fh $save_text;
+    close($fh);
+    print "Done as $$\.txt\n";
+}
+
+sub find_module_place {
+    my ($module) = @_;
+    
+    $module =~ s/\:\:/\//isg;
+    $module .= '.pm';
+    foreach my $dir (@INC) {
+        return "$dir/$module" if (-e "$dir/$module");
+    }
+}
+
+sub get_sub_modules {
+    my ($module) = @_;
+    
+    my $dir = $module;
+    $dir =~ s/\.pm/\//isg;
+
+    my @modules;
+    my $files = File::Next::files( $dir );
+    while ( defined ( my $file = $files->() ) ) {
+        push @modules, $file if ($file =~ /\.pm/);
+    }
+
+    return @modules;
+}
+
+1;
