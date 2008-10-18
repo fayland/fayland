6cd0b4fbae185aa2f6fec02f72c072dfaf0bc811
fayland <fayland@gmail.com>
2008-10-18 03:09:31 +0000

    learn_ppi/01DoubleToSingle.pl

---
+++ script/learn_ppi/01DoubleToSingle.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+use PPI;
+
+my $string = do { local $/; <DATA> };
+my $doc = PPI::Document->new(\$string);
+
+my $to;
+foreach my $ele ( @{ $doc->find('PPI::Token') } ) {
+    #print Dumper(\$ele);
+
+    if ( $ele->isa('PPI::Token::Quote::Double') ) {
+        unless ( $ele->interpolations ) {
+            my $str = $ele->string;
+            my $separator = $ele->{separator};
+            if ( $str !~ /\'/) {
+                $to .= "'$str'";
+            } else {
+                if ( $str !~ /\~/ ) {
+                    $to .= 'q~' . $str . '~';
+                } else {
+                    $to .= $ele->content;
+                }
+            }
+            next;
+        }
+    } elsif ( $ele->isa('PPI::Token::Quote::Interpolate') ) {
+        my $str = $ele->string;
+        my $separator = $ele->{separator};
+        $to .= "q$separator" . $str . $separator;
+        next;
+    }
+    $to .= $ele->content;
+}
+
+print $to;
+
+__DATA__
+my $a = "a";
+my $b = "'b'";
+my $c = qq~c~;
+my $d = qq~'d'~;
+my $e = q{e};
+my $f = 'f';
+if ( $a eq "a" ) {
+    print "ok";
+}
