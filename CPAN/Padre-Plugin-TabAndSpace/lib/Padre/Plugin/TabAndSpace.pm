2eb4d2b8a578e4f5f94f94f3eeebdd1bad160d4d
fayland <fayland@gmail.com>
2008-11-03 04:06:20 +0000

    requires Padre '0.15'

--- CPAN/Padre-Plugin-TabAndSpace/lib/Padre/Plugin/TabAndSpace.pm
+++ CPAN/Padre-Plugin-TabAndSpace/lib/Padre/Plugin/TabAndSpace.pm
@@ -3,7 +3,7 @@ package Padre::Plugin::TabAndSpace;
 use warnings;
 use strict;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Wx ':everything';
 use Padre::Wx::History::TextDialog;
--- CPAN/Padre-Plugin-WordStats/lib/Padre/Plugin/WordStats.pm
+++ CPAN/Padre-Plugin-WordStats/lib/Padre/Plugin/WordStats.pm
@@ -3,7 +3,7 @@ package Padre::Plugin::WordStats;
 use warnings;
 use strict;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Wx ':everything';
 
