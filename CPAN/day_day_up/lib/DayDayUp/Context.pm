31c433c16d195d70eba6225427f831a69b22d876
fayland <fayland@gmail.com>
2008-11-17 09:31:06 +0000

    add dbi support.

--- CPAN/day_day_up/lib/DayDayUp/Context.pm
+++ CPAN/day_day_up/lib/DayDayUp/Context.pm
@@ -5,6 +5,7 @@ use Moose;
 extends 'Mojolicious::Context';
 
 use YAML qw/LoadFile/;
+use DBI;
 
 has 'config' => (
 	is => 'ro',
@@ -19,6 +20,19 @@ has 'config' => (
 			return {};
 		}
 		return YAML::LoadFile($file);
+	}
+);
+
+has 'dbh' => (
+	is => 'ro',
+	lazy => 1,
+	default => sub {
+		my $self = shift;
+		my $home = $self->app->home;
+		my $app  = $home->app_class;
+		my $db_file = $home->rel_file( lc($app) . '.db' );
+		return DBI->connect("dbi:SQLite:dbname=$db_file", '', '', {RaiseError => 1})
+			or die $DBI::err;
 	}
 );
 
