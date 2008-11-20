88446d55ad80b1abb7eacafa225c122411d963d3
fayland <fayland@gmail.com>
2008-11-20 06:38:50 +0000

    Padre::Plugin::AcmePlayCode 0.04

--- CPAN/Padre-Plugin-AcmePlayCode/lib/Padre/Plugin/AcmePlayCode.pm
+++ CPAN/Padre-Plugin-AcmePlayCode/lib/Padre/Plugin/AcmePlayCode.pm
@@ -3,9 +3,7 @@ package Padre::Plugin::AcmePlayCode;
 use warnings;
 use strict;
 
-our $VERSION = '0.03';
-
-use Acme::PlayCode;
+our $VERSION = '0.04';
 
 my @menu = (
     ['Averything',        \&averythig],
@@ -23,6 +21,7 @@ sub menu {
 sub _play {
 	my ( $self, $plugin ) = @_;
 	
+	require Acme::PlayCode;
     my $playapp = new Acme::PlayCode;	
 	$playapp->load_plugin( $plugin );
 	
