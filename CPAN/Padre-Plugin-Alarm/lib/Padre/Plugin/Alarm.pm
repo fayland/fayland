e9c1e876126e70ade6d4b7f41dcaec1848ea26ad
fayland <fayland@gmail.com>
2008-12-07 03:08:43 +0000

    Alarm.pm

--- CPAN/Padre-Plugin-Alarm/lib/Padre/Plugin/Alarm.pm
+++ CPAN/Padre-Plugin-Alarm/lib/Padre/Plugin/Alarm.pm
@@ -10,7 +10,6 @@ use Wx         ':everything';
 use Wx::Event  ':everything';
 use Wx::Locale qw(:default);
 use Padre::Wx::Dialog ();
-use constant SECONDS => 1000;
 use vars qw/$alarm_timer_id/;
 
 sub padre_interfaces {
@@ -22,13 +21,14 @@ sub menu_plugins_simple {
 	# check if we need set timer on
 	my $plugin_manager = Padre->ide->plugin_manager;
 	my $config = $plugin_manager->plugin_config('Alarm');
-	if ( $config and scalar @$config ) {
+	if ( $config and exists $config->{alarms} and scalar @{$config->{alarms}} ) {
 		_set_alarm();
 	}
 	
 	return ('Alarm Clock' => [
 		'Set Alarm Time', \&set_alarm_time,
 		'Stop Alarm',     \&stop_alarm,
+		'Clear Alarm',    \&clear_alarm,
 	]);
 }
 
@@ -43,7 +43,7 @@ sub set_alarm_time {
 		],
 		[
 			[ 'Wx::StaticText', undef,              gettext('Frequency:')],
-			[ 'Wx::ComboBox',   '_frequency_',     '', \@frequency],
+			[ 'Wx::ComboBox',   '_frequency_',     'once', \@frequency],
 		],
 		[
 			[ 'Wx::Button',     '_ok_',           Wx::wxID_OK     ],
@@ -82,7 +82,7 @@ sub ok_clicked {
 	$dialog->Destroy;
 	
 	my $main_window = Padre->ide->wx->main_window;
-	
+
 	my $alarm_time = $data->{_alarm_time_};
 	if ( $alarm_time !~ /^\d{1,2}\:\d{2}$/ ) {
 		Wx::MessageBox(gettext('Possible Value Format: \d:\d\d or \d\d:\d\d like 6:13 or 23:55'), gettext("Wrong Alarm Time"), Wx::wxOK, $main_window);
@@ -90,11 +90,12 @@ sub ok_clicked {
 	}
 	
 	my $frequency = $data->{_frequency_};
-	
+
 	my $plugin_manager = Padre->ide->plugin_manager;
 	my $config = $plugin_manager->plugin_config('Alarm');
-	
-	push @$config, {
+	$config->{alarms} ||= [];
+
+	push @{$config->{alarms}}, {
 		time => $alarm_time,
 		frequency => $frequency,
 		status => 'enabled',
@@ -103,23 +104,37 @@ sub ok_clicked {
 }
 
 sub _set_alarm {
-	my ( $main ) = @_;
-	
+	my $main = Padre->ide->wx->main_window;
+
 	$alarm_timer_id = Wx::NewId unless $alarm_timer_id;
+
 	my $timer = Wx::Timer->new( $main, $alarm_timer_id );
 	unless ( $timer->IsRunning ) {
 		Wx::Event::EVT_TIMER($main, Padre::Wx::id_FILECHK_TIMER, \&on_timer_alarm);
-		$timer->Start(60 * SECONDS, 0); # every minute
+		$timer->Start(60 * 1000, 0); # every minute
 	}
 }
 
 sub stop_alarm {
-	my ( $main ) = shift;
+	my $main = shift;
 	return unless $alarm_timer_id;
 	my $timer = Wx::Timer->new( $main, $alarm_timer_id );
 	if ( $timer->IsRunning ) {
 		$timer->Stop();
 	}
+	
+	$main->message('All Alarms are stopped', 'Stop Alarm');
+}
+
+sub clear_alarm {
+	my $main = shift;
+	
+	my $plugin_manager = Padre->ide->plugin_manager;
+	my $config = $plugin_manager->plugin_config('Alarm');
+	$config->{alarms} = [];
+	
+	$main->stop_alarm();
+	$main->message('All Alarms are cleared', 'Clear Alarm');
 }
 
 sub on_timer_alarm {
@@ -128,28 +143,32 @@ sub on_timer_alarm {
 	my $plugin_manager = Padre->ide->plugin_manager;
 	my $config = $plugin_manager->plugin_config('Alarm');
 	
-	return unless $config;
+	return unless ( $config and exists $config->{alarms} );
 	
 	# get now-time;
 	my @ntime = localtime();
-	my $ntime = sprintf('%d:%2d', $ntime[2], $ntime[1]);
+	my $ntime = sprintf('%d:%02d', $ntime[2], $ntime[1]);
 	
-	foreach ( @$config ) {
+	my $did_something = 0;
+	foreach ( @{$config->{alarms}} ) {
 		return unless ( $_->{status} eq 'enabled' );
 		# check if it's the time
 		my $time = $_->{time};
 		$time =~ s/^0//;
 
 		if ( $time eq $ntime ) {
+			$did_something = 1;
 			my $frequency = $_->{frequency};
-			Wx::MessageBox('Play Music', 'llllll', Wx::wxOK, $main_window);
+			play_music();
 			if ( $frequency eq 'once' ) {
 				$_->{status} = 'disabled';
 			}
 		}
 	}
 	
-	$config = [ grep { $_->{status} eq 'enabled' } @$config ];
+	if ( $did_something ) {
+		$config->{alarms} = [ grep { $_->{status} eq 'enabled' } @{$config->{alarms}} ];
+	}
 }
 
 sub play_music {
